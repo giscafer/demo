c953d78d43c4fd9ee3719b3c5266283a5d0c5fae
giscafer <giscafer@outlook.com>
2019-04-16 11:43:32 +0800

    add puppeteer demo

---
+++ puppeteer-demo/picture.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://github.com/giscafer');
+  await page.setViewport({
+    width: 1440,
+    height: 960,
+  })
+  await page.screenshot({ path: 'giscafer.png' });
+  await page.pdf({ path: 'giscafer.pdf' });
+  await browser.close();
+})();
