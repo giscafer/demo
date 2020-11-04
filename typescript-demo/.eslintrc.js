8b8fc5997c510d3d25f238aba9ba53c279f534ea
giscafer <giscafer@outlook.com>
2020-11-04 20:19:05 +0800

    chore: add event-loop demo

--- typescript-demo/.eslintrc.js
+++ typescript-demo/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
-  exclude: ['node_modules', 'ocp/**/*.ts'],
+  ignorePatterns: ['node_modules', 'ocp/**/*.ts'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
