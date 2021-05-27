6c5633b2a33b55577bd797ad1ed0bc28f5b3ba8b
giscafer <giscafer@outlook.com>
2021-05-27 17:35:31 +0800

    pull

--- js-snippets/pull.js
+++ js-snippets/pull.js
@@ -25,7 +25,36 @@ export const pullBy = (arr, ...args) => {
   pulled.forEach((v) => arr.push(v));
 };
 
-var myArray = [{ x: 1 }, { x: 2 }, { x: 3 }, { x: 1 }];
+// var myArray = [{ x: 1 }, { x: 2 }, { x: 3 }, { x: 1 }];
+// pullBy(myArray, [{ x: 1 }, { x: 3 }], (o) => o.x);
+// console.log(myArray);
 
-pullBy(myArray, [{ x: 1 }, { x: 3 }], (o) => o.x);
-console.log(myArray);
+export const pullAtIndex = (arr, ...args) => {
+  const argState = Array.isArray(args[0]) ? args[0] : args;
+  let removed = [];
+  let pulled = arr
+    .map((v, i) => (argState.includes(i) ? removed.push(v) : v))
+    .filter((v, i) => !argState.includes(i));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+  return removed;
+};
+
+// let myArray = ['a', 'b', 'c', 'd'];
+// let pulled = pullAtIndex(myArray, [1, 3]);
+// myArray = [ 'a', 'c' ] , pulled = [ 'b', 'd' ]
+
+export const pullAtValue = (arr, ...args) => {
+  const argState = Array.isArray(args[0]) ? args[0] : args;
+  const removed = [];
+  const pulled = arr
+    .map((v) => (argState.includes(v) ? (removed.push(v), v) : v))
+    .filter((v) => !argState.includes(v));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+  return removed;
+};
+
+// let myArray = ['a', 'b', 'c', 'd'];
+// let pulled = pullAtValue(myArray, ['b', 'd']);
+// myArray = [ 'a', 'c' ] , pulled = [ 'b', 'd' ]
